7c82f1a7d49becc3231da0f85c01e1b565949ba6
Magdalena Skarbinska <mskarbinska@protonmail.com>
2023-05-06 18:37:15 +0200

    update timezone, set superuser

--- api/models.py
+++ api/models.py
@@ -4,6 +4,5 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
