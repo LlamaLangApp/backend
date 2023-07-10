c903b0513238a0f09631f70c575d98e3b3f6bb54
Michał Gniadek <mgniadek@interia.pl>
2023-07-10 21:41:04 +0200

    Ask questions and analyze answers

--- api/consumers.py
+++ api/consumers.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, asdict
 from enum import Enum, auto
 import json
 from random import shuffle, sample
-from typing import List
+from typing import Any, List, Tuple
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -54,16 +54,20 @@ class SocketGameState(Enum):
 class WaitListConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         if self.scope["user"].is_authenticated:
-            await self.accept()
             self.user: User = self.scope["user"]
             self.state = SocketGameState.JUST_CONNECTED
+            await self.accept()
         else:
+            self.user = None
+            self.state = None
             await self.close()
 
     async def disconnect(self, close_code):
+        if not self.state:
+            return
+
         if self.state == SocketGameState.IN_WAITROOM:
             await self.remove_user_from_waitroom()
-
         elif self.state == SocketGameState.IN_GAME:
             await self.on_disconnected()
 
@@ -82,17 +86,19 @@ class WaitListConsumer(AsyncWebsocketConsumer):
             message = WaitroomRequestMessage(game=message["game"])
 
             room_pk = await self.add_user_to_waitroom(message.game)
-            group_name = str(room_pk)
-            await self.channel_layer.group_add(group_name, self.channel_name)
+            self.group_name = str(room_pk)
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
 
             await self.send(JoinedWaitroomMessage().to_json())
             self.state = SocketGameState.IN_WAITROOM
 
-            players = await self.is_waitroom_full(message.game)
+            is_full, players = await self.is_waitroom_full(message.game)
 
-            if players:
+            if is_full:
                 session_id = await self.on_game_init(players)
-                await self.channel_layer.group_send(group_name, {"type": "start.game", "session_id": session_id})
+                await self.channel_layer.group_send(
+                    self.group_name, {"type": "start.game", "session_id": session_id}
+                )
         elif self.state == SocketGameState.IN_WAITROOM:
             pass
         elif self.state == SocketGameState.IN_GAME:
@@ -108,13 +114,13 @@ class WaitListConsumer(AsyncWebsocketConsumer):
         return room.pk
 
     @database_sync_to_async
-    def is_waitroom_full(self, game) -> bool:
+    def is_waitroom_full(self, game) -> Tuple[bool, Any]:
         room = WaitingRoom.objects.filter(game=game).get()
         if len(room.users.all()) == 2:
-            players = room.users.all()
+            players = list(room.users.all().values_list("pk", flat=True))
             room.delete()
-            return players
-        return False
+            return True, players
+        return False, None
 
     async def start_game(self, event):
         await self.on_start(event["session_id"])
@@ -135,6 +141,7 @@ class WaitListConsumer(AsyncWebsocketConsumer):
 class RaceMessageType(str, Enum):
     NEW_QUESTION = "new_question"
     RESPONSE = "response"
+    RESULT = "result"
 
 
 @dataclass
@@ -150,15 +157,30 @@ class AnswerMessage(WebSocketMessage):
     type: str = RaceMessageType.RESPONSE
 
 
+@dataclass
+class ResultMessage(WebSocketMessage):
+    correct: str
+    points: int
+    type: str = RaceMessageType.RESULT
+
+
 class RaceConsumer(WaitListConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.race_session = None
 
     async def on_game_init(self, players):
-        race_rounds = get_race_rounds(await database_sync_to_async(get_words_for_play)())
-        game_session = RaceActiveGame.objects.create(users=players, rounds=race_rounds)
-        return game_session.pk
+        return (await self.create_race_active_game(players)).pk
+
+    @database_sync_to_async
+    def create_race_active_game(self, players):
+        race_rounds = get_race_rounds(get_words_for_play())
+        game_session = RaceActiveGame.objects.create(
+            rounds=json.dumps([asdict(r) for r in race_rounds])
+        )
+        game_session.users.set(players)
+        game_session.save()
+        return game_session
 
     async def on_start(self, session_id):
         self.state = SocketGameState.BEFORE_GAME
@@ -166,48 +188,88 @@ class RaceConsumer(WaitListConsumer):
         message = await self.create_starting_message()
         await self.send(message)
 
-        await asyncio.sleep(5)
+        await asyncio.sleep(1)
 
         self.state = SocketGameState.IN_GAME
-        await self.send(self.get_new_question_message().to_json())
+        await self.send(await self.create_question_message())
 
     async def on_message(self, message):
-        if message.type == RaceMessageType.RESPONSE:
-            self.handle_answer(message)
+        message = AnswerMessage(answer=message["answer"])
+
+        if await self.handle_answer(message):
+            await self.channel_layer.group_send(
+                self.group_name, {"type": "send.results"}
+            )
 
     async def on_disconnected(self):
         pass
 
-    def get_new_question_message(self):
-        return NewQuestionMessage(question="B", answers=["a", "b", "c", "d"])
+    async def send_results(self, event):
+        await self.send(await self.create_result_message())
+        await asyncio.sleep(1)
+        await self.send(await self.create_question_message())
+
+    @database_sync_to_async
+    def create_question_message(self):
+        rounds = json.loads(self.race_session.rounds)
+        if self.race_session.round_count < len(rounds):
+            round = rounds[self.race_session.round_count]
+            return NewQuestionMessage(
+                question=round["question"], answers=round["options"]
+            ).to_json()
+        else:
+            print("end")
 
     @database_sync_to_async
     def create_starting_message(self):
         players = self.race_session.users.all().values_list("username", flat=True)
-        GameStartingMessage(players=players).to_json()
+        return GameStartingMessage(players=list(players)).to_json()
+
+    @database_sync_to_async
+    def create_result_message(self):
+        self.race_session.refresh_from_db()
+        round = json.loads(self.race_session.rounds)[self.race_session.round_count - 1]
+        points = 0
+        if round["answer"] == self.last_answer:
+            points = 15
+        return ResultMessage(correct=round["answer"], points=points).to_json()
 
     @database_sync_to_async
     def get_race_session(self, session_id):
         self.race_session = RaceActiveGame.objects.get(pk=session_id)
 
-    def handle_answer(self, message):
-        user_id, answer = message["user_id"], message["answer"]
+    @database_sync_to_async
+    def handle_answer(self, message: AnswerMessage):
+        self.last_answer = message.answer
+        self.race_session.refresh_from_db()
         self.race_session.answers_count += 1
+        self.race_session.save()
+
+        if self.race_session.answers_count == len(self.race_session.users.all()):
+            self.race_session.answers_count = 0
+            self.race_session.round_count += 1
+            self.race_session.save()
+            return True
+
+        return False
 
 
-
-def get_race_rounds(words):
+def get_race_rounds(words) -> List[RaceRound]:
     rounds = []
-    for _ in range(10):
+    for _ in range(5):
         shuffle(words)
         word = words.pop(0)
         correct_translation = word["polish"]
 
         incorrect_translations = sample([w["polish"] for w in words], 3)
 
-        rounds.append(RaceRound(answer=correct_translation,
-                                question=word["english"],
-                                options=incorrect_translations + [correct_translation]))
+        rounds.append(
+            RaceRound(
+                answer=correct_translation,
+                question=word["english"],
+                options=incorrect_translations + [correct_translation],
+            )
+        )
     return rounds
 
 
