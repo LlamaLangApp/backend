5d2f46a585e1df3c15a0ad3b246d3fcf3d69e4d7
Magdalena Skarbinska <mskarbinska@protonmail.com>
2023-11-12 17:22:02 +0100

    add customization

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-
-python manage.py migrate
-python manage.py create_user user1 pass
-python manage.py create_user user2 pass
-python manage.py create_user user3 pass
-python manage.py runserver 0.0.0.0:8000
+#!/bin/shpython manage.py migratepython manage.py create_user user1 passpython manage.py create_user user2 passpython manage.py create_user user3 passpython manage.py runserver 0.0.0.0:8000
